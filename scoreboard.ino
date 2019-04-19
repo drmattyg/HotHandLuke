92d556025498dcee29f655b2279ec5e42670a2b7
Matthew Gordon <inverarity@gmail.com>
2019-04-18 20:37:34 -0700

    Checkpoing

--- scoreboard.ino
+++ scoreboard.ino
@@ -13,6 +13,21 @@ int rx_state = LOW;
 int rx_state_new;
 int currentMcpOutput = 0;
 
+const int DIGIT0[] PROGMEM = {0, 1, 2, 3, 4, 6};
+const int DIGIT1[] PROGMEM = {5, 2};
+const int DIGIT2[] PROGMEM = {6, 5, 3, 1, 0};
+const int DIGIT3[] PROGMEM = {6, 5, 3, 2, 0};
+const int DIGIT4[] PROGMEM = {5, 4, 3, 2};
+const int DIGIT5[] PROGMEM = {6, 4, 3, 2, 0};
+const int DIGIT6[] PROGMEM = {6, 4, 3, 2, 1, 0};
+const int DIGIT7[] PROGMEM = {6, 5, 2};
+const int DIGIT8[] PROGMEM = {6, 5, 4, 3, 2, 1, 0};
+const int DIGIT9[] PROGMEM = {6, 5, 4, 3, 2, 0};
+
+
+
+
+
 void setup() {  
   mcp.begin();      // use default address 0
   for(int i = 0; i < 16; i++) {
