0f576296b6cb478812c8fc5b14dd7de137c891ed
Matthew Gordon <inverarity@gmail.com>
2019-04-18 21:07:57 -0700

    Checkpoint

--- scoreboard.ino
+++ scoreboard.ino
@@ -8,24 +8,49 @@
 #define RX2 2
 #define RX3 3
 
+
+
+
 Adafruit_MCP23017 mcp;
 int rx_state = LOW;
 int rx_state_new;
 int currentMcpOutput = 0;
 
-const int DIGIT0[] PROGMEM = {0, 1, 2, 3, 4, 6};
-const int DIGIT1[] PROGMEM = {5, 2};
-const int DIGIT2[] PROGMEM = {6, 5, 3, 1, 0};
-const int DIGIT3[] PROGMEM = {6, 5, 3, 2, 0};
-const int DIGIT4[] PROGMEM = {5, 4, 3, 2};
-const int DIGIT5[] PROGMEM = {6, 4, 3, 2, 0};
-const int DIGIT6[] PROGMEM = {6, 4, 3, 2, 1, 0};
-const int DIGIT7[] PROGMEM = {6, 5, 2};
-const int DIGIT8[] PROGMEM = {6, 5, 4, 3, 2, 1, 0};
-const int DIGIT9[] PROGMEM = {6, 5, 4, 3, 2, 0};
+// relays are numbered starting from 1, so RELAYS[0] is nothing
+// RELAYS[1] is the mcp pin for relay 1
+
+const int RELAYS[] PROGMEM = [100, 8, 7, 14, 1, 13, 2, 12, 3, 11, 4, 10, 5, 9, 6, 15, 0]
+
+const char DIGIT0[] PROGMEM = {0, 1, 2, 3, 4, 6};
+const char DIGIT1[] PROGMEM = {5, 2};
+const char DIGIT2[] PROGMEM = {6, 5, 3, 1, 0};
+const char DIGIT3[] PROGMEM = {6, 5, 3, 2, 0};
+const char DIGIT4[] PROGMEM = {5, 4, 3, 2};
+const char DIGIT5[] PROGMEM = {6, 4, 3, 2, 0};
+const char DIGIT6[] PROGMEM = {6, 4, 3, 2, 1, 0};
+const char DIGIT7[] PROGMEM = {6, 5, 2};
+const char DIGIT8[] PROGMEM = {6, 5, 4, 3, 2, 1, 0};
+const char DIGIT9[] PROGMEM = {6, 5, 4, 3, 2, 0};
+const char* DIGITS[] PROGMEM = {DIGIT0, DIGIT1, DIGIT2, DIGIT3, DIGIT4, DIGIT5, DIGIT6, DIGIT7, DIGIT8, DIGIT9};
+
+int currentDigitVal0[] = {-1, -1};
 
 
+void sevenSeg(int digitNum, int digitVal, int state) {
+  for(int i = 0; i < sizeof(DIGITS[digitVal])) {
+    int relayNum = DIGITS[digitVal] + 1 + (digitNum * 10);
+    mcp.digitalWrite(RELAYS[relayNum], state);
+  }
+}
 
+void setDigit(int digitNum, int digitVal) {
+  // turn off previous digit
+  if(currentDigitVal[digitNum] != -1) {
+    sevenSeg(currentDigitVal[digitNum], HIGH);
+  }
+  sevenSeg(digitNum, digitVal, LOW);
+  currentDigitVal[digitNum] = digitVal;
+}
 
 
 void setup() {  
