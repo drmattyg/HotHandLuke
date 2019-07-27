576a28a31a8a1459b0a5f670cccd9339d311c43a
Matt Gordon <inverarity@gmail.com>
2019-07-26 20:53:26 -0700

    cleanup

--- scoreboard/scoreboard.ino
+++ scoreboard/scoreboard.ino
@@ -9,6 +9,8 @@
 #define RX3 11
 
 
+
+
 Adafruit_MCP23017 mcp;
 int rx_state = LOW;
 int rx_state_new;
@@ -20,37 +22,58 @@ int currentMcpOutput = 0;
 // relays are numbered starting from 1, so RELAYS[0] is nothing
 // RELAYS[1] is the mcp pin for relay 1
 
-const int RELAYS[] PROGMEM = {100, 8, 7, 14, 1, 13, 2, 12, 3, 11, 4, 10, 5, 9, 6, 15, 0};
+const int RELAYS[] = {100, 8, 7, 14, 1, 13, 2, 12, 3, 11, 4, 10, 5, 9, 6, 15, 0};
 
-const char DIGIT0[] PROGMEM = {0, 1, 2, 3, 4, 6};
-const char DIGIT1[] PROGMEM = {5, 2};
-const char DIGIT2[] PROGMEM = {6, 5, 3, 1, 0};
-const char DIGIT3[] PROGMEM = {6, 5, 3, 2, 0};
-const char DIGIT4[] PROGMEM = {5, 4, 3, 2};
-const char DIGIT5[] PROGMEM = {6, 4, 3, 2, 0};
-const char DIGIT6[] PROGMEM = {6, 4, 3, 2, 1, 0};
-const char DIGIT7[] PROGMEM = {6, 5, 2};
-const char DIGIT8[] PROGMEM = {6, 5, 4, 3, 2, 1, 0};
-const char DIGIT9[] PROGMEM = {6, 5, 4, 3, 2, 0};
+const char DIGIT0[] = {0, 1, 2, 4, 5, 6};
+const char DIGIT1[] = {5, 2};
+const char DIGIT2[] = {6, 5, 3, 1, 0};
+const char DIGIT3[] = {6, 5, 3, 2, 0};
+const char DIGIT4[] = {5, 4, 3, 2};
+const char DIGIT5[] = {6, 4, 3, 2, 0};
+const char DIGIT6[] = {6, 4, 3, 2, 1, 0};
+const char DIGIT7[] = {6, 5, 2};
+const char DIGIT8[] = {6, 5, 4, 3, 2, 1, 0};
+const char DIGIT9[] = {6, 5, 4, 3, 2, 0};
 const char* DIGITS[] = {DIGIT0, DIGIT1, DIGIT2, DIGIT3, DIGIT4, DIGIT5, DIGIT6, DIGIT7, DIGIT8, DIGIT9};
-
+const char* DIGIT_LEN[] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
 int currentDigitVal[] = {-1, -1};
 
 
 void sevenSeg(int digitNum, int digitVal, int state) {
-  for(int i = 0; i < sizeof(DIGITS[digitVal]); i++) {
-    int relayNum = DIGITS[digitVal] + 1 + (digitNum * 10);
+  char buff[50];
+  sprintf(buff, "digitNum %d, digitVal %d, state %d, digit len %d", digitNum, digitVal, state, DIGIT_LEN[digitVal]);
+  Serial.println(buff);
+  char* digit_to_write = DIGITS[digitVal];
+  for(int i = 0; i < DIGIT_LEN[digitVal]; i++) {
+    int relayNum = digit_to_write[i] + 1 + (digitNum * 8);
+    sprintf(buff, "digitVal %d, relayNum %d", digitVal, RELAYS[relayNum]);
+    Serial.println(buff);
     mcp.digitalWrite(RELAYS[relayNum], state);
   }
 }
 
+void clearAll() {
+  for(int i = 1; i < 17; i++) {
+    mcp.digitalWrite(RELAYS[i], HIGH);
+  }
+}
+
+void clearDigit(int digit) {
+  int relayOffset;
+  if(digit == LOW) {
+    relayOffset = 9;
+  } else {
+    relayOffset = 1;
+  }
+  for(int i = 0; i < 8; i++) {
+    mcp.digitalWrite(RELAYS[relayOffset + i], HIGH);
+  }
+}
 void setDigit(int digitNum, int digitVal) {
-  sevenSeg(digitNum, digitVal, LOW);
 
   // turn off previous digit
-  if(currentDigitVal[digitNum] != -1) {
-    sevenSeg(digitNum, currentDigitVal[digitNum], HIGH);
-  }
+  sevenSeg(digitNum, digitVal, LOW);
+
   currentDigitVal[digitNum] = digitVal;
 }
 
@@ -59,12 +82,13 @@ void incrementSevenSeg() {
   if(currentDigitVal[1] == 9) {
     currentDigitVal[1] = 0;
     currentDigitVal[0]++;
-    setDigit(1, 0);
-    setDigit(0, currentDigitVal[0]);
+    clearDigit(HIGH);
   } else {
     currentDigitVal[1]++;
-    setDigit(1, currentDigitVal[1]);
   }
+  clearDigit(LOW);
+  setDigit(1, currentDigitVal[1]);
+  setDigit(0, currentDigitVal[0]);
 }
 
 
@@ -79,7 +103,10 @@ void setup() {
   for(int i = 8; i <= 12; i++) {
     pinMode(i, INPUT); // RC RX pins
   }
-  Serial.begin(115200);
+  Serial.begin(9600);
+  clearAll();
+  setDigit(1, 0);
+  setDigit(0, 0);
 }
 
 void flashLED(int n) {
@@ -90,80 +117,39 @@ void flashLED(int n) {
     delay(FLASH_TIME);
   }
 }
+int j = 0;
 
 void loop() {
-
-  int buttA = digitalRead(RX0);
-  int buttB = digitalRead(RX1);
-  int buttC = digitalRead(RX2);
-  int buttD = digitalRead(RX3);
+  // read input as int to debounce
+  int buttD = digitalRead(RX0);
+  int buttC = digitalRead(RX1);
+  int buttB = digitalRead(RX2);
+  int buttA = digitalRead(RX3);
 
   //increment by 1
-  if(buttA == 1){
-    flashLED(1);
-    incrementSevenSeg();
-    delay(500);
-    Serial.println(currentDigitVal[0]);
-    Serial.println(currentDigitVal[1]);
-  }
-  //increment by 3
   if(buttB == 1){
     flashLED(1);
     incrementSevenSeg();
-    incrementSevenSeg();
-    incrementSevenSeg();
-    delay(500);
-    Serial.println(currentDigitVal[0]);
-    Serial.println(currentDigitVal[1]);
+    delay(500); //for debounce
+    //Serial.println(currentDigitVal[0]);
+    //Serial.println(currentDigitVal[1]);
+  }
+  //increment by 3
+  if(buttC == 1){
+    delay(500); //for debounce
   }
   //increment by 5
-  if(buttC == 1){
-    flashLED(1);
-    incrementSevenSeg();
-    incrementSevenSeg();
-    incrementSevenSeg();
-    incrementSevenSeg();
-    incrementSevenSeg();
-    delay(500);
-    Serial.println(currentDigitVal[0]);
-    Serial.println(currentDigitVal[1]);
-      
+  if(buttD == 1){
+    delay(500); //for debounce
   }
   //reset
-  if(buttD == 1){
+  if(buttA == 1){
     flashLED(2);
     setDigit(0,0);
     setDigit(1,0);
-    delay(500);
-    Serial.println(currentDigitVal[0]);
-    Serial.println(currentDigitVal[1]);
+    delay(500); //for debounce
+    //Serial.println(currentDigitVal[0]);
+    //Serial.println(currentDigitVal[1]);
   }
- /* 
-  bool rx_changed[] = {false, false, false, false};
-  rx_state = digitalRead(RX_ACTIVE);
-  for(int i = 0; i < 4; i++) {
-      rx_pin_current[i] = digitalRead(RX_PINS[i]);
-      if(rx_pin_current[i] != rx_pin_prev[i]) {
-        rx_changed[i] = true;
-        rx_pin_prev[i] = rx_pin_current[i];
-        flashLED(i);
-      }
-  }
-  if(rx_changed[RX0] && rx_pin_current[RX0]) {
-    // RX0 = increment
-    incrementSevenSeg();
-    Serial.println(currentDigitVal[0]);
-    Serial.println(currentDigitVal[1]);
-  } else if(rx_changed[RX1] && rx_pin_current[RX1]) {
-    // RX1 = set to zero
-    setDigit(0, 0);
-    setDigit(1, 0);
-  }
-
-  if(rx_state != rx_state_new && rx_state) {
-    rx_state = rx_state_new;
-    //flashLED();
-  }
-*/ 
-
+ 
 } //end loop
